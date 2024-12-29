42d4fdeb5561a66078144e65aebba1db2af56e5d
Daveboiii <dorvensdave@gmail.com>
2024-12-29 18:03:18 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "dave"
 
-global.banknumber = "8133729715"
+global.banknumber = "50955623994"
 
-global.bankowner = "FADARE"
+global.bankowner = "dorvensley"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
